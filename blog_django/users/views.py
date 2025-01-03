bb5b4bfc6150f8c59085674c16f21f4fc207d3a6
snowflakes640 <maliha5640@gmail.com>
2025-01-03 20:06:36 +0600

    Login & Logout System

--- blog_django/users/views.py
+++ blog_django/users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from .forms import UserRegisterForm
 
@@ -9,8 +10,8 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get("username")
-            messages.success(request, f"Account created for {username}")
-            return redirect("blog-home")
+            messages.success(request, f"Account Created. REady to Login?")
+            return redirect("login")
         # else:
         #     print("Form is not valid.")
         #     print(form.errors)
@@ -18,7 +19,10 @@ def register(request):
     else:
         form = UserRegisterForm()
     return render(request, "users/register.html", {"form": form})
-    
+
+@login_required   
+def profile(request):
+    return render(request, "users/profile.html")
 
 
 # def register(request):
